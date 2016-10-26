3a4b12cbc26db65831038906ecfa8c4c964f442e
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-26 20:08:31 +0100

    Added explicit dependency from the database to the consulserver. It should be implicit because of the use of aws_instance.consulserver.private_ip but just in case...

--- aws_resources.tf
+++ aws_resources.tf
@@ -40,6 +40,7 @@ resource "aws_instance" "consulserver" {
 
 resource "aws_instance" "database" {
   ami                         = "${lookup(var.aws_amis, var.aws_region)}"
+  depends_on                  = ["aws_instance.consulserver"]
   instance_type               = "t2.micro"
   subnet_id                   = "${aws_subnet.vpc_subnet.id}"
   vpc_security_group_ids      = ["${aws_security_group.vpc.id}"]
