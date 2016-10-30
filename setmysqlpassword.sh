bc229a4e497e29b1accf1f5145aee3c8243d5901
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-30 20:46:19 +0000

    Trim double quotes in the Auth token

--- setmysqlpassword.sh
+++ setmysqlpassword.sh
@@ -2,7 +2,7 @@
 VAULTHOST=$1
 /bin/curl -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 > /bin/jq
 chmod +x /bin/jq
-AUTH_TOKEN=$(/bin/curl -X POST -k "https://$VAULTHOST:8200/v1/auth/aws-ec2/login" -d '{"role":"example","pkcs7":"'$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d '\n')'"}' | tee /tmp/response | /bin/jq .auth.client_token)
+AUTH_TOKEN=$(/bin/curl -X POST -k "https://$VAULTHOST:8200/v1/auth/aws-ec2/login" -d '{"role":"example","pkcs7":"'$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d '\n')'"}' | tee /tmp/response | /bin/jq .auth.client_token | tr -d \")
 if [ "$?" -eq "0" ] && [ ! -z "$AUTH_TOKEN" ]; then
   sleep 10
   MYSQL_PASSWORD=$(/bin/curl -H "X-Vault-Token: $AUTH_TOKEN" -k -X GET https://$VAULTHOST:8200/v1/mysql/config/connection | tee /tmp/response2 | /bin/jq .data.connection_url | awk -F\: '{print $2}' | awk -F\@ '{print $1}')
