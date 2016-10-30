f50758eeafa58e17e61baaa9dd679f1ccf5f2d47
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-30 19:42:32 +0000

    When things dont make sense, sleep 10

--- setmysqlpassword.sh
+++ setmysqlpassword.sh
@@ -4,6 +4,7 @@ VAULTHOST=$1
 chmod +x /bin/jq
 AUTH_TOKEN=$(/bin/curl -X POST -k "https://$VAULTHOST:8200/v1/auth/aws-ec2/login" -d '{"role":"example","pkcs7":"'$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d '\n')'"}' | tee /tmp/response | /bin/jq .auth.client_token)
 if [ "$?" -eq "0" ] && [ ! -z "$AUTH_TOKEN" ]; then
+  sleep 10
   MYSQL_PASSWORD=$(/bin/curl -H "X-Vault-Token: $AUTH_TOKEN" -k -X GET https://$VAULTHOST:8200/v1/mysql/config/connection | tee /tmp/response2 | /bin/jq .data.connection_url | awk -F\: '{print $2}' | awk -F\@ '{print $1}')
     if [ "$?" -eq "0" ] && [ ! -z "$MYSQL_PASSWORD" ]; then
       /bin/mysql -u root mysql -e "SET PASSWORD FOR 'vault'@'%' = PASSWORD('$MYSQL_PASSWORD'); FLUSH PRIVILEGES;"
